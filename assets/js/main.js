3df209744d454b95c891f7f6ebee7a707fef2d45
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-17 18:34:12 +0100

    Esercizio completato grezzo ma funzionante

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,57 @@
 // Permettiamo la modifica di un todo già aggiunto.
 $(document).ready(function(){
 //Al click sul bottone
-$('#add').click(function () {
-  //Prendo il valore dell'input
-  var title = $('#insert-title').val();
-  //Svuoto l'input mettendo stringa vuota
-  $('#insert-title').val('');
-  
-  //Clono il li del template
-  var newTemplate = $('#template li').clone();
-})
-})//Chiusura ready
+  $('#add').click(function(){
+    //Prendo il valore dell'input
+    var inputTitle = $('#input-title').val();
+    //Svuoto l'input mettendo stringa vuota
+    $('#insert-title').val('');
+
+    //Clono il li del template
+    var template = $('#template li').clone();
+    //Seleziono il title e inserisco il valore
+    template.children('span.title').text(inputTitle);
+
+    //Al click rimuovo interno degli span
+    template.children('span.delete').click(function(){
+      $(this).parent().remove();
+    });
+
+    //Al click su Modifica do la possibilità di modificare
+    template.children('span.edit').click(function(){
+      var title = $(this).siblings('span.title').text();
+      $(this).siblings('span.title').hide();
+
+      $(this).siblings('input').val(title).show();
+    });
+
+    template.children('input').keyup(function(e){
+      if (e.which == 13) {
+        var title = $(this).val();
+        // console.log($(this));
+        $(this).hide();
+        $(this).siblings('span.title').text(title).show();
+      }
+    });
+
+    $('#to-do-list').append(template);
+  });
+
+  // // //Abilito la possibilità di inserire utilizzando invio della tastiera
+  // $('input').keyup(function(e){
+  //   console.log(e);
+  //   if (e.which == 13) {
+  //     var inputTitle = $(this).val();
+  //     console.log(e);
+  //     var template = $('#template li').clone();
+  //     template.children('span.title').text(inputTitle);
+  //     //Svuoto l'input mettendo stringa vuota
+  //     // $('#input-title').val('');
+  //   } else {
+  //     console.log('non lo prendo');
+  //   }
+  // })
+
+
+
+});//Chiusura ready
