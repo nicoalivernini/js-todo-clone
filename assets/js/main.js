a6b493c4f1ce1bf6e6a7b68c4cf2679125fc7219
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-17 15:47:05 +0100

    Raccolta dati

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,45 @@
 // Rendiamo il tutto gradevole alla vista.
 // Bonus
 // Permettiamo la modifica di un todo già aggiunto.
-var toDoList = $('#to-do-list')
+var toDoList = $('#to-do-list');
 
-var title = $('#title')
+var title = $('#title');
 
-var modifica = $('#edit')
+var scrivi = $('#modify')
 
-var cancella = $('#delete')
+var modifica = $('#edit');
+
+var cancella = $('#delete');
+
+var input = $('#add');
+
+// console.log(toDoList, title, modifica, cancella);
+
+$(document).ready(function(){
+
+  $('#sent').click(function(){
+    var title = $('#add').val();
+    console.log(title);
+    $('#add').val('');
+  })
+
+  var cloneLi = $('#to-do-list ul li');
+  console.log(cloneLi);
+  cloneLi.children('#title').text(title);
+
+
+}) //Chiusura ready
+
+
+
+
+
+
+
+// $('#add').keyup(function(e) {
+//   if (e.which == 13) {
+//     var title = $('#add').val();
+//     $('#title').append(title);
+//     $('#add').val('');
+//   }
+// })
