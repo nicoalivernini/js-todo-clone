4c3e175837a77b5854b4b57c5614dff2d30a80b1
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-17 15:02:50 +0100

    Creazione file base

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Creare una Todo-list
+// in cui si possano aggiungere nuovi todo e rimuoverli.
+// Rendiamo il tutto gradevole alla vista.
+// Bonus
+// Permettiamo la modifica di un todo già aggiunto.
+var toDoList = $('#to-do-list')
+
+var title = $('#title')
+
+var modifica = $('#edit')
+
+var cancella = $('#delete')
